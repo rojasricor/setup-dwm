7f318a10fd92ca94a69b4fad4f7586591fff1287
Richard A. Rich <rojasricor@gmail.com>
2024-02-22 09:52:06 -0500

    Improving script

--- setup-dwm.sh
+++ setup-dwm.sh
@@ -1,17 +1,20 @@
+echo "Activate root superuser access:.."
+su
+
 echo "Setting comunity repo, and searching fast repos..."
-sudo setup-apkrepos -cf
+setup-apkrepos -cf
 
 echo "Updating system..."
-sudo apk update && sudo apt upgrade
+apk update && sudo apt upgrade
 
 echo "Installing utils: neofetch, redshift..."
-sudo apk add neofetch redshift
+apk add neofetch redshift
 
 echo "Installing xorg..."
-sudo setup-xorg-base
+setup-xorg-base
 
 echo "Installing dependencies..."
-sudo apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 chromium font-noto-emoji
+apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 chromium font-noto-emoji
 
 echo "Installing suckless software by git..."
 cd /tmp
@@ -19,18 +22,22 @@ echo "Clonning dwm git repo..."
 git clone https://git.suckless.org/dwm
 cd dwm
 echo "Installing dwm..."
-sudo make clean install
+make clean install
 cd ..
 echo "Clonning dmenu git repo..."
 git clone https://git.suckless.org/dmenu
 cd dmenu
 echo "Installing dmenu..."
-sudo make clean install
+make clean install
 cd ..
 echo "Clonning st terminal git repo..."
 git clone https://git.suckless.org/st
 cd st
 echo "Installing st terminal..."
-sudo make clean install
+make clean install
+
+echo "Exiting from root user..."
+exit
+echo "Setting xorg and start init dwm basic config files..."
 mv .profile .xinitrc ~/
 
