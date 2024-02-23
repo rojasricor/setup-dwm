799d0e42b120a18374a9d440eb27952871ec3e2b
Richard A. Rich <rojasricor@localhost.my.domain>
2024-02-23 14:25:59 -0500

    Fixing bugs

--- setup-dwm.sh
+++ setup-dwm.sh
@@ -1,50 +1,62 @@
+#!/bin/bash
+
 repopath=$(pwd)
 echo $repopath
 
-echo "Setting comunity repo, and searching fast repos..."
-#setup-apkrepos -cf
-
-echo "Updating system..." 
+repos() {
+  echo "Setting community repo, and searching fast repos..."
+  sudo setup-apkrepos -cf
+}
+repos
+w
+echo "Updating system..."
 sudo apk update
 sudo apk upgrade
-
-echo "Installing utils: neofetch, redshift..."
-sudo apk add neofetch redshift setxkbmap
 
 echo "Installing xorg..."
 sudo setup-xorg-base
 
 echo "Installing dependencies..."
-sudo apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 chromium font-noto-emoji
+sudo apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 firefox-esr font-noto-emoji
 # Ferdev27 dwm dotfiles dependencies
-sudo apk add libxrandr-dev imlib2-dev linux-headers
+sudo apk add libxrandr-dev imlib2-dev linux-headers coreutils brightnessctl pulseaudio pulseaudio-alsa pulseaudio-utils scrot neofetch setxkbmap redshift font-ubuntu-mono-nerd font-noto-emoji libnotify dunst cbatticon volumeicon
 
-echo "Installing suckless software by git..."
-cd /tmp
-echo "Clonning dwm git repo..."
-git clone https://git.suckless.org/dwm
-cd dwm
-echo "Installing dwm..."
-sudo make clean install
-cd .. 
-echo "Clonning dmenu git repo..." 
-git clone https://git.suckless.org/dmenu
-cd dmenu 
-echo "Installing dmenu..." 
-sudo make clean install 
-cd .. 
-echo "Clonning st terminal git repo..." 
-git clone https://git.suckless.org/st
-cd st 
-echo "Installing st terminal..." 
-sudo make clean install 
+installSucklessSoft() {
+  echo "Installing suckless software via git..."
+  cd /tmp || exit
+  echo "Cloning dwm git repo..."
+  git clone https://git.suckless.org/dwm
+  cd dwm || exit
+  echo "Installing dwm..."
+  sudo make clean install
+  cd ..
+  echo "Cloning dmenu git repo..."
+  git clone https://git.suckless.org/dmenu
+  cd dmenu || exit
+  echo "Installing dmenu..."
+  sudo make clean install
+  cd ..
+  echo "Cloning st terminal git repo..."
+  git clone https://git.suckless.org/st
+  cd st || exit
+  echo "Installing st terminal..."
+  sudo make clean install
+  cd ..
+}
+
+# installSucklessSoft
 
 echo "Enter your username:.."
 read usrname
-echo "Setting xorg and start init dwm basic config files..." 
-cd $repopath
-cp -r ./.profile ./.xinitrc /home/$usrname
+echo "Setting xorg and starting initializing dwm basic config files..."
+cd "$repopath" || exit
+cp -r ./.profile ./.xinitrc "/home/$usrname"
 
-echo "Adding user to groups..."
-sudo adduser $usrname wheel
-sudo adduser $usrname input
+addNeededGroups() {
+  echo "Adding user: $usrname to needed groups..."
+  sudo adduser "$usrname" wheel
+  sudo adduser "$usrname" input
+  sudo adduser "$usrname" audio
+  sudo adduser "$usrname" video
+}
+addNeededGroups
